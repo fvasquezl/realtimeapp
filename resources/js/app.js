3db226d7d2b4f664d506eba46054573623cd0aae
faustino_vasquez <faustino.vasquez@mitechnologiesinc.com>
2020-05-12 02:27:45 -0700

    Helper Class and functions

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,9 @@ window.Vue = require('vue');
 import Vuetify from 'vuetify'
 Vue.use(Vuetify)
 
+import User from './Helpers/User'
+window.User = User
+
 Vue.component('AppHome',require('./components/AppHome').default);
 
 import router from './Router/router';
