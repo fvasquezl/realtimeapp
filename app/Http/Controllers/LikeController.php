0ccef63b6c6bb5b9b7a4d65a0b5f640cccaea8e9
faustino_vasquez <faustino.vasquez@mitechnologiesinc.com>
2020-05-22 00:58:38 -0700

    realtime likes, and replies

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-
+use App\Events\LikeEvent;
 use App\Model\Reply;
 use Illuminate\Http\Request;
 
@@ -23,13 +23,14 @@ class LikeController extends Controller
     {
         $reply->like()->create([
            'user_id' => auth()->id()
-          // 'user_id' => 1
         ]);
+
+        broadcast(new LikeEvent($reply->id,1))->toOthers();
     }
 
     public function unLikeIt(Reply $reply)
     {
        $reply->like()->where('user_id',auth()->id())->first()->delete();
-      //  $reply->like()->where('user_id',1)->first()->delete();
+       broadcast(new LikeEvent($reply->id,0))->toOthers();
     }
 }
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,6 +18,12 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        \App\Events\LikeEvent::class =>[
+            \App\Listeners\LikeEventListener::class
+        ],
+        \App\Events\DeleteReplyEvent::class =>[
+            \App\Listeners\DeleteReplyEventListener::class
+        ]
     ];
 
     /**
